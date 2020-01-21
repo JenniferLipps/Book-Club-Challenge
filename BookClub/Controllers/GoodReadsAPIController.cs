6ba728c76a30213972680c14c3266774c9b9ef9b
Jennifer Lipps <jjlipps@gmail.com>
2020-01-20 21:25:56 -0600

    API returns data, GR client library is working

---
+++ BookClub/Controllers/GoodReadsAPIController.cs
@@ -0,0 +1,44 @@
+﻿using BookClub.Models;
+using Goodreads;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BookClub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoodReadsAPIController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get(string ISBN="9780297859383")
+        {
+            //var client = new RestClient("https://www.goodreads.com");
+            //client.UseDotNetXmlSerializer();
+
+            var client = GoodreadsClient.Create("hG8Baz6PR4BYmKheAD3qg", "0nyS2sEH9JKSWil0j5xOBdcB6BzLmxscoo8yAIk");
+
+            // Now you are able to call some Goodreads endpoints that don't need the OAuth credentials. For example:
+            // Get a book by specified id.
+            var book = await client.Books.GetByIsbn(ISBN);
+
+
+            //var serializer = new XmlSerializer(typeof(ApiResponse));
+
+            //ApiResponse result;
+
+            //using (TextReader reader = new StringReader(response.Content))
+            //{
+            //    result
+            //}
+
+
+            return Ok();
+        }
+    }
+}
--- BookClub/Startup.cs
+++ BookClub/Startup.cs
@@ -26,6 +26,13 @@ namespace BookClub
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddCors(options => options.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

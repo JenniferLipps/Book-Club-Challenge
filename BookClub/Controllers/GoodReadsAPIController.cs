3172fa9d1fa856377d22dce32d1b945bb4549bf0
Jennifer Lipps <jjlipps@gmail.com>
2020-01-25 13:46:00 -0600

    search by title and ISBN work

--- BookClub/Controllers/GoodReadsAPIController.cs
+++ BookClub/Controllers/GoodReadsAPIController.cs
@@ -15,19 +15,45 @@ namespace BookClub.Controllers
     [ApiController]
     public class GoodReadsAPIController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<string>>> Get(string ISBN="9780297859383")
+        [HttpGet("{isbn}")]
+        public async Task<ActionResult<IEnumerable<string>>> Get(string isbn)
         {
-            //var client = new RestClient("https://www.goodreads.com");
-            //client.UseDotNetXmlSerializer();
+            const string apiKey = "hG8Baz6PR4BYmKheAD3qg";
+            const string apiSecret = "0nyS2sEH9JKSWil0j5xOBdcB6BzLmxscoo8yAIk";
 
-            var client = GoodreadsClient.Create("hG8Baz6PR4BYmKheAD3qg", "0nyS2sEH9JKSWil0j5xOBdcB6BzLmxscoo8yAIk");
+            var client = GoodreadsClient.Create(apiKey, apiSecret);
 
-            // Now you are able to call some Goodreads endpoints that don't need the OAuth credentials. For example:
-            // Get a book by specified id.
-            var book = await client.Books.GetByIsbn(ISBN);
+            var book = await client.Books.GetByIsbn(isbn);
 
-            return Ok();
+            return Ok(book);
+        }
+
+        [HttpGet("search/{title}")]
+        public async Task<List<GoodReadsBookDTO>> SearchByTitle(string title)
+        {
+            const string apiKey = "hG8Baz6PR4BYmKheAD3qg";
+            const string apiSecret = "0nyS2sEH9JKSWil0j5xOBdcB6BzLmxscoo8yAIk";
+
+            var client = GoodreadsClient.Create(apiKey, apiSecret);
+
+            var books = await client.Books.Search(title, 1, Goodreads.Models.Request.BookSearchField.Title);
+
+            var goodReadsBooks = new List<GoodReadsBookDTO>();
+
+            foreach (var book in books.List)
+            {
+                var goodReadsBook = new GoodReadsBookDTO()
+                {
+                    Author = book.BestBook.AuthorName,
+                    Title = book.BestBook.Title,
+                    ImageURL = book.BestBook.ImageUrl,
+                    GoodReadsBookId = book.BestBook.Id
+                };
+
+                goodReadsBooks.Add(goodReadsBook);
+            }
+
+            return goodReadsBooks;
         }
     }
 }
--- BookClub/DataAccess/BookRepository.cs
+++ BookClub/DataAccess/BookRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookClub.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace BookClub.DataAccess
 {
     public class BookRepository
     {
+        //public List<Book> GetAll()
     }
 }

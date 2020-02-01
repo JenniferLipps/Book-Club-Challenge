608f9b8f205ad65ef9298d8e6d879e9c9c4c7eb5
Jennifer Lipps <jjlipps@gmail.com>
2020-01-31 22:32:53 -0600

    started book POST

--- BookClub/Models/Book.cs
+++ BookClub/Models/Book.cs
@@ -7,9 +7,13 @@ namespace BookClub.Models
 {
     public class Book
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         //public int ISBN { get; set; }
         public Uri Image { get; set; }
+        public long GoodReadsBookId { get; set; }
+        public int UserId { get; set; }
+        public DateTime DateCompleted { get; set; }
     }
 }

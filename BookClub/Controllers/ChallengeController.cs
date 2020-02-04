89159dca6345589ff397f4cd0d934396630f1d3c
Jennifer Lipps <jjlipps@gmail.com>
2020-02-03 21:29:02 -0600

    create challenge is working

---
+++ BookClub/Controllers/ChallengeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookClub.Commands;
+using BookClub.DataAccess;
+using BookClub.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookClub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChallengeController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddChallenge(AddChallengeCommand addChallengeCommand)
+        {
+            //create Challenge
+            var repo = new ChallengeRepository();
+            var startDate = DateTime.Now;
+            var endDate = DateTime.Now;
+            var newChallenge = repo.AddChallenge(startDate, endDate);
+
+            //add users to Challenge
+            foreach (var userId in addChallengeCommand.userIds)
+            {
+                repo.AddUserToChallenge(addChallengeCommand.creatorId, userId, newChallenge.Id);
+
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("{userId}")]
+        public IEnumerable<Challenge> GetChallengesByUser(int userId)
+        {
+            var repo = new ChallengeRepository();
+            return repo.GetChallengesByUser(userId);
+        }
+
+        [HttpPost("{userId}/{challengeId}")]
+        public IActionResult AddUserToChallenge(int userId, int challengeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookClub/DataAccess/ChallengeRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BookClub.Models;
+using Dapper;
+
+namespace BookClub.DataAccess
+{
+    public class ChallengeRepository
+    {
+        string _connectionString = "Server=localhost; Database=BookClubChallenge; Trusted_Connection=True;";
+
+        public Challenge AddChallenge(DateTime startDate, DateTime endDate)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"insert into [dbo].[Challenge]
+	                        ([StartDate]
+	                        ,[EndDate]
+	                        )
+                            output inserted.*
+                            values (
+                            @startDate
+                            ,@endDate)";
+
+                var parameters = new
+                {
+                    StartDate = startDate,
+                    EndDate = endDate
+                };
+
+                var newChallenge = db.QueryFirst<Challenge>(sql, parameters);
+
+                return newChallenge;
+            }
+        }
+
+        internal void AddUserToChallenge(int creatorId, int userId, int challengeId)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"insert into [dbo].[UserChallenge]
+                            ([ChallengeId]
+                            ,[UserId])
+                            values
+                            (@challengeId
+                            ,@userId)";
+
+                var parameters = new
+                {
+                    ChallengeId = challengeId,
+                    UserId = userId
+                };
+
+
+                db.Execute(sql, parameters);
+            }
+
+
+        }
+
+        public IEnumerable<Challenge> GetChallengesByUser(int userId)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"select * from [UserChallenge] where Id = @id";
+
+                var parameters = new
+                {
+                    Id = userId
+                };
+
+                var challenges = db.Query<Challenge>(sql, parameters);
+
+                return challenges;
+            }
+        }
+
+    }
+}
